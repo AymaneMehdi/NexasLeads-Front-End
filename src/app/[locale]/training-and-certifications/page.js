b7c0f557c63c6984a2a2e575dc7fbe428694963e
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-24 19:53:46 +0100

    Add TrainingAndCertifications component with metadata for SEO and page structure

---
+++ src/app/[locale]/training-and-certifications/page.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Head from "next/head";
+
+export const metadata = {
+  title: "NexasLeads - Training & Certifications",
+  description: "Master business development with expert training. Boost your team's sales skills and close more deals with proven techniques.",
+  keywords: "email marketing, go to market, growth hacking, demand generation, B2B Growth, lead generation, appointment setting, LinkedIn marketing, WhatsApp marketing, SMS marketing",
+  icons: {
+    icon: ['/favicon.ico?v=1'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  }
+};
+const TrainingAndCertifications = () => {
+  return (
+    <div>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+    </div>
+  );
+};
+export default TrainingAndCertifications;

e694231ec1dfa684925f26d6f20e7fa1a22c3d71
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-24 19:22:48 +0100

    Add internationalization support with middleware and i18n configuration
    - Created i18n.js for handling locale-specific messages.
    - Implemented middleware.js to manage locale routing.
    - Added Tailwind CSS configuration for styling.
    - Introduced global styles in globals.css for consistent design.

---
+++ src/app/[locale]/layout.js
@@ -0,0 +1,17 @@
+import "@/layouts/globals.css";
+export default function RootLayout({ children, params }) {
+  const { locale } = params;
+  return (
+    <html lang={locale}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/[locale]/page.js
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: "NexasLeads",
+  description:
+    "Demand Generation for B2B companies, Outbound Marketing campaigns , Lead Generation and appointment setting to Generate You Meetings And Clients.",
+  keywords:
+    "email marketing, go to market, growth hacking, demand generation, B2B Growth, lead generation, appointment setting, LinkedIn marketing, WhatsApp marketing, SMS marketing",
+  icons: {
+    icon: ["/favicon.ico?v=1"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+const HomePage = () => {
+  return <div></div>;
+};
+
+export default HomePage;

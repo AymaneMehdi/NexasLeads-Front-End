ea8f775048a96a22b5f48c7583587b38bc86043b
Aymane Mehdi <yorubornin1999@gmail.com>
2026-04-24 20:04:43 +0100

    Add ContactUs component with SEO metadata and page structure

---
+++ src/app/[locale]/ContactUs/page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Head from "next/head";
+
+export const metadata = {
+  title: "NexasLeads - Contact Us",
+  description: "Get tailored B2B growth strategies. Reach out for a free consultation and discover how we can accelerate your business success.",
+  keywords: "email marketing, go to market, growth hacking, demand generation, B2B Growth, lead generation, appointment setting, LinkedIn marketing, WhatsApp marketing, SMS marketing",
+  icons:{
+    icon:['/favicon.ico?v=1'],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  }
+};
+const ContactUs = () => {
+  return (
+  <div>
+  <Head>
+    <title>{metadata.title}</title>
+    <meta name="description" content={metadata.description} />
+    <meta name="keywords" content={metadata.keywords} />
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+  </Head>
+  </div>
+  );
+};
+
+export default ContactUs;

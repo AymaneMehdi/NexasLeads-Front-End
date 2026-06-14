a31b7949de8616032095501e2448bfc53e41e031
Aymane Mehdi <yorubornin1999@gmail.com>
2026-06-14 21:23:32 +0100

    Add Contact Us page with metadata, layout, and component integration.

---
+++ src/app/[locale]/ContactUs/page.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import FAQ from "@/components/FAQ";
+import ContactUsPage from "@/components/ContactUsPage";
+
+export const metadata = {
+  title: "NexasLeads - Contact Us",
+  description:
+    "Get tailored B2B growth strategies. Reach out for a free consultation and discover how we can accelerate your business success.",
+  keywords:
+    "email marketing, go to market, growth hacking, demand generation, B2B Growth, lead generation, appointment setting, LinkedIn marketing, WhatsApp marketing, SMS marketing",
+  icons: {
+    icon: ["/favicon.ico?v=1"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+const ContactUs = () => {
+  return (
+    <div>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <Navbar />
+      <div className="mt-28">
+        <ContactUsPage />
+      </div>
+      <FAQ />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactUs;
